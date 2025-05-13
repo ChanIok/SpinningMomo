ffce089bec8e7eaf552d7fbbb99e322193b2fb5e
ChanIok <chaniok@qq.com>
2025-05-13 23:47:04 +0800

    chore: 更新版本号

--- src/version.hpp
+++ src/version.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 // 版本号定义
-#define VERSION_NUM     0,7,6,0
-#define VERSION_STR     "0.7.6.0"
+#define VERSION_NUM     0,7,7,0
+#define VERSION_STR     "0.7.7.0"
 
 // 项目信息
 #define PRODUCT_NAME     "SpinningMomo"
