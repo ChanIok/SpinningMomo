8d63da0586d36bb21084bcd9b19bd39d59ccc986
ChanIok <10853091+ChanIok@users.noreply.github.com>
2026-07-29 02:01:14 +0800

    chore: 更新版本至2.1.4

--- src/core/version.hpp
+++ src/core/version.hpp
@@ -4,6 +4,6 @@
 
 namespace core::version {
 
-inline auto get_app_version() -> std::string { return "2.1.3.0"; }
+inline auto get_app_version() -> std::string { return "2.1.4.0"; }
 
 }  // namespace core::version
